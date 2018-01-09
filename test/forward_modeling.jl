049cabfcf3393c5be8166ceb27340fe2dbf82454
WneLeiGao <wgao1@ualberta.ca>
2018-01-08 21:09:00 -0700

    make model for GSA presentation

--- test/forward_modeling.jl
+++ test/forward_modeling.jl
@@ -1,30 +1,57 @@
 using PyPlot, ElasticWave
 
-nz = 200; nx = 300; # model size
+nz = 300; nx = 300; # model size
 ext= 20; # thickness of PML layers
-iflag = 2; #when iflag=1, four side obsorbing boundary, iflag=2, free surface
-dx = 10. ; dz = 10. ; dt = 1e-3; # grid size
-tmax = 6; f0=10.; # recording time and dominant frequency of source wavelet
-vp=3000.*ones(nz,nx);  vs=3000./sqrt(3)*ones(nz,nx); rho=2.5*ones(nz,nx);
-vp[150:end,:]=4000.;  vs[150:end,:]=4000./sqrt(3); #physical model
+iflag = 1; #when iflag=1, four side obsorbing boundary, iflag=2, free surface
+dx = 5. ; dz = 5. ; dt = 4e-4; # grid size
+tmax = 1.5; f0=20.; # recording time and dominant frequency of source wavelet
+vp=2500.*ones(nz,nx); vs=2500./sqrt(3)*ones(nz,nx); rho=2.5*ones(nz,nx);
+vp[151:end,:]=3500.;  vs[151:end,:]=3500./sqrt(3.); #physical model
+SeisPlot(vp, xlabel="X (km)", ylabel="Z (km)", vmin=1500, vmax=4500, cmap="gray", dx=0.005, dy=0.005);
+tight_layout(); savefig("/Users/wenyue/Desktop/v.pdf")
 fidMtx = CreateFidMtx(nz, nx, ext, iflag, vp, vs, rho, dz, dx, dt, f0); #finite difference matrix
 
 # single source
-isz = 1; isx = 150; ot = 0.0; #source location and starting time
+isz = 5; isx = 150; ot = 0.0; #source location and starting time
 flags = vec([false false true true false]); # add source to normal stress, simulate explosive sources
 src = InitSource(isz, isx, nz, nx, ext, iflag, f0, ot, dt, flags);
-
+src.Txx = src.Txx * 1e5; src.Tzz = src.Tzz * 1e5;
 
 
 # # =========finite difference modeling==========
 # irx = collect(1:nx); irz = 1*ones(Int64, length(irx)); #location of receivers
 # shotv = MultiStepForward(irz, irx, src, fidMtx, tmax=tmax) #generate one shot
+root = homedir(); path = join([root "/Desktop/wfd.bin"]);
+MultiStepForward(path, src, fidMtx, tmax=tmax);
+
+root = homedir(); path = join([root "/Desktop/decomposed.bin"]);
+MultiStepForward(path, src, fidMtx, dz, dx; tmax=tmax)
 
 root = homedir(); path = join([root "/Desktop/wfd.bin"]);
 MultiStepForward(path, src, fidMtx, tmax=tmax);
-path1 = join([root "/Desktop/moive"]);
-waveAnim(path1, path, cpt="Vz")
 
+(nz, nx, ext, iflag, dt, nt) = InfoPv(path);
+pp = zeros(nt); ps = zeros(nt);
+rz = irz[1] + ext; rx = irx[1] + ext; idx = (rx-1)*(nz+2*ext) + rz + ext;
+for i = 1151 : nt
+    (vxx, vxz, vzx, vzz) = ReadPv(path, i)
+    pp[i] = vxx[idx] + vzz[idx]
+    ps[i] = vxz[idx] - vzx[idx]
+end
+
+path1 = join([root "/Desktop/moive"]);
+waveAnim(path1, path, cpt="Vz", cmap="gray", wbox=6, hbox=6, dt1=0.003, pclip=1.0, vlim=0.1)
+
+irz = [1]; irx = [250];
+shotv = binWfd2shotv(irz, irx, path)
+
+obs = hcat(shotv.Vz, shotv.Vx); obs[1:1150,:] = 0.0;
+spt = hcat(pp, ps);
+SeisPlot(obs, style="wiggles", dy=0.0004, xcur=0.8, ylabel="Time (s)", xticks=[]);
+tight_layout(); savefig("/Users/wenyue/Desktop/obs.pdf");
+
+SeisPlot(spt, style="wiggles", dy=0.0004, xcur=0.8, ylabel="Time (s)", xticks=[]);
+tight_layout(); savefig("/Users/wenyue/Desktop/spt.pdf");
 
 
 # function MultiStepForward_old(irz::Array{Int64,1}, irx::Array{Int64,1}, src::Source, fidMtx::FidMtx; tmax=0.5)
